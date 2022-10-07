f2bfebb658d5bdf5eaad326cf36c44208f568fec
caiop3 <caio.ribeiro.p3@gmail.com>
2022-10-07 12:05:10 -0300

    update: views.py

--- notes/views.py
+++ notes/views.py
@@ -6,13 +6,20 @@ db = Database('notes')
 
 def index(request):
     if request.method == 'POST':
-        title = request.POST.get('titulo')
-        content = request.POST.get('detalhes')
-        note = Note(title=title, content=content)
-        note.save()
-        db.add(Note(title,content))
-        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
-        return redirect('index')
+        if request.POST.get("verb") == "add":
+            title = request.POST.get('titulo')
+            content = request.POST.get('detalhes')
+            note = Note(title=title, content=content)
+            note.save()
+            db.add(Note(title,content))
+            # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+            return redirect('index')
+        elif request.POST.get("verb") == "delete":
+            title = request.POST.get('titulo')
+            for note in Note.objects.all():
+                if note.title == title:
+                    id = note.id
+                    note.delete(id)
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
