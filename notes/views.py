75efa86815edaf881e74040ee9d993969d666143
caiop3 <caio.ribeiro.p3@gmail.com>
2022-10-08 00:15:27 -0300

    update: same

--- notes/views.py
+++ notes/views.py
@@ -11,7 +11,7 @@ def index(request):
             content = request.POST.get('detalhes')
             note = Note(title=title, content=content)
             note.save()
-            # db.add(Note(title,content))
+            # db.add(Note (title,content))
             # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
             return redirect('index')
         elif request.POST.get("verb") == "delete":
@@ -20,7 +20,7 @@ def index(request):
                 if note.title == title:
                     note.delete()
                     # db.delete(note.id)
-                    break
+                    return redirect('index')
             # id = request.POST.get('id')
             # for note in Note.objects.all():
             #     if id == note.id:
